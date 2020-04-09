db2d4cb0283cca0c1c3bb9fe71f3e9beb76ab149
Armoteo <armoteo@ukr.net>
2020-04-09 17:10:11 +0300

    item list

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import {StyleSheet, Text, View} from 'react-native'
+import {Navbar} from './src/Navbar'
+import {AddTodo} from './src/AddTodo'
+import {Todo} from './src/Todo'
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (title) => {
+    setTodos((prevTodos) => [
+      ...prevTodos,
+      {
+        id: Date.now().toString,
+        title: title,
+      },
+    ])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>HELLO WORLD!</Text>
+    <View>
+      <Navbar title={'Перечень задач'} />
+      <View style={styles.container}>
+        <AddTodo onSubmit={addTodo} />
+        <View>
+          {todos.map((todo) => (
+            <Todo key={todo.id} todo={todo} />
+          ))}
+        </View>
+      </View>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 30,
+    paddingVertical: 30,
   },
-});
+})
